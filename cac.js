e4de8651610e175c21997fd7074fb04f54809a8e
uncjav <53953453+uncjav@users.noreply.github.com>
2019-10-05 23:56:41 +0700

    Add files via upload

---
+++ cac.js
@@ -0,0 +1,93 @@
+﻿	
+feed_instance_id	"ea87e197-743a-4cd1-a2ee-cf9b3b0a7657"
+title	"Driving USA"
+kind	"Single Item"
+playlist	
+0	
+mediaid	"8L4m9FJB"
+description	"Beautiful sunset point of view shot along empty desert highway through Monument Valley, Arizona Utah"
+pubdate	1495054284
+title	"Driving USA"
+image	"https://cdn.jwplayer.com/v2/media/8L4m9FJB/poster.jpg?width=720"
+tags	"stock video"
+variations	
+images	
+0	
+image	"https://cdn.jwplayer.com/v2/media/8L4m9FJB/thumbnails/fr14p5y4.jpg?width=720"
+id	"fr14p5y4"
+weight	0.143
+1	
+image	"https://cdn.jwplayer.com/v2/media/8L4m9FJB/thumbnails/lcim4qft.jpg?width=720"
+id	"lcim4qft"
+weight	0.073
+2	
+image	"https://cdn.jwplayer.com/v2/media/8L4m9FJB/thumbnails/04vlmghw.jpg?width=720"
+id	"04vlmghw"
+weight	0.781
+3	
+image	"https://cdn.jwplayer.com/v2/media/8L4m9FJB/thumbnails/slashs0k.jpg?width=720"
+id	"slashs0k"
+weight	0.003
+images	
+0	
+src	"https://cdn.jwplayer.com/v2/media/8L4m9FJB/poster.jpg?width=320"
+type	"image/jpeg"
+width	320
+1	
+src	"https://cdn.jwplayer.com/v2/media/8L4m9FJB/poster.jpg?width=480"
+type	"image/jpeg"
+width	480
+2	
+src	"https://cdn.jwplayer.com/v2/media/8L4m9FJB/poster.jpg?width=640"
+type	"image/jpeg"
+width	640
+3	
+src	"https://cdn.jwplayer.com/v2/media/8L4m9FJB/poster.jpg?width=720"
+type	"image/jpeg"
+width	720
+tracks	
+0	
+kind	"thumbnails"
+file	"https://cdn.jwplayer.com/strips/8L4m9FJB-120.vtt"
+link	"https://cdn.jwplayer.com/previews/8L4m9FJB"
+duration	30
+sources	
+0	
+type	"application/dash+xml"
+mediaTypes	
+0	"video/webm; codecs=\"vp9\""
+file	"https://cdn.jwplayer.com/manifests/8L4m9FJB.mpd"
+1	
+type	"application/vnd.apple.mpegurl"
+file	"https://cdn.jwplayer.com/manifests/8L4m9FJB.m3u8"
+2	
+width	320
+height	180
+type	"video/mp4"
+file	"https://lh3.googleusercontent.com/W6LSStAeOHFWxqt0uXXTc7wDhpKfTZa63eewCftzSBO-ID1getFTe3OZHvRtnPWDHj1fLfEFefYDQ07eu9U=m18"
+label	"H.264 320px"
+3	
+width	480
+height	270
+type	"video/mp4"
+file	"https://lh3.googleusercontent.com/W6LSStAeOHFWxqt0uXXTc7wDhpKfTZa63eewCftzSBO-ID1getFTe3OZHvRtnPWDHj1fLfEFefYDQ07eu9U=m18"
+label	"H.264 480px"
+4	
+width	720
+height	406
+type	"video/mp4"
+file	"https://lh3.googleusercontent.com/W6LSStAeOHFWxqt0uXXTc7wDhpKfTZa63eewCftzSBO-ID1getFTe3OZHvRtnPWDHj1fLfEFefYDQ07eu9U=m18"
+label	"H.264 720px"
+5	
+width	1280
+height	720
+type	"video/mp4"
+file	"https://lh3.googleusercontent.com/W6LSStAeOHFWxqt0uXXTc7wDhpKfTZa63eewCftzSBO-ID1getFTe3OZHvRtnPWDHj1fLfEFefYDQ07eu9U=m22"
+label	"H.264 1280px"
+6	
+width	1920
+height	1080
+type	"video/mp4"
+file	"https://lh3.googleusercontent.com/W6LSStAeOHFWxqt0uXXTc7wDhpKfTZa63eewCftzSBO-ID1getFTe3OZHvRtnPWDHj1fLfEFefYDQ07eu9U=m37"
+label	"H.264 1920px"
+description	"Beautiful sunset point of view shot along empty desert highway through Monument Valley, Arizona Utah"
